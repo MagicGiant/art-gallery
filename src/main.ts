926b45bc7988554a0e76938ce0961ad266bd8c7c
Sherka <obidjnaow@mail.ru>
2024-05-11 14:06:23 +0300

    make it working

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import * as hbs from 'hbs';
+import * as path from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.setBaseViewsDir('views/partials');
+  hbs.registerPartials(path.join(__dirname, '..', 'views', 'partials')); 
+
+  app.setBaseViewsDir('views');
   app.setViewEngine('hbs');
+
+  // app.set('view options', { layout: 'layouts/main' }); 
 
   const port = 3000;
   await app.listen(port);
